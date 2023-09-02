fba1f887a9288b7e83bb3d38d14ee20aed2c5ffb
jumtras <data0science0acc@gmail.com>
2023-09-02 02:34:40 +0000

    feature : pre texttiling

--- agenda_maker/model/segmentation/model_semantic_text_segmentation.py
+++ agenda_maker/model/segmentation/model_semantic_text_segmentation.py
@@ -3,8 +3,6 @@ from logging import getLogger
 from typing import Dict, List
 
 import numpy as np
-import spacy
-from nltk.tokenize.texttiling import TextTilingTokenizer
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from transformers import pipeline
@@ -13,6 +11,7 @@ from agenda_maker.common.release_gpu_memory import release_gpu_memory
 from agenda_maker.model.base_model import BaseModel
 
 logger = getLogger(__name__)
+
 
 # Reffrence :https://github.com/maxent-ai/converse
 class SemanticTextSegmentation(BaseModel):
@@ -26,18 +25,10 @@ class SemanticTextSegmentation(BaseModel):
 
     def set_params(self) -> None:
         # parameter of  semantic segmentation
-        self.threshold = (
-            self.config_manager.config.model.segmentation.semantic_segmentation.threshold
-        )
-        self.threshold_step = (
-            self.config_manager.config.model.segmentation.semantic_segmentation.threshold_step
-        )
-        self.max_segment_text = (
-            self.config_manager.config.model.segmentation.semantic_segmentation.max_segment_text
-        )
-        self.model_type = (
-            self.config_manager.config.model.segmentation.semantic_segmentation.model_type
-        )
+        self.threshold = self.config_manager.config.model.segmentation.semantic_segmentation.threshold
+        self.threshold_step = self.config_manager.config.model.segmentation.semantic_segmentation.threshold_step
+        self.max_segment_text = self.config_manager.config.model.segmentation.semantic_segmentation.max_segment_text
+        self.model_type = self.config_manager.config.model.segmentation.semantic_segmentation.model_type
 
     def build_model(self) -> None:
         self.set_params()
@@ -45,7 +36,6 @@ class SemanticTextSegmentation(BaseModel):
         self.load_spacy()
 
     def get_result(self, list_text: List[str]) -> list:
-
         self.max_segment_text = self._get_max_segment_text(list_text=list_text)
         semantic_segmentation = self._semantic_segmentation(list_text, self.threshold)
         release_gpu_memory(gpu_task=self.model)
@@ -59,9 +49,7 @@ class SemanticTextSegmentation(BaseModel):
     def load_spacy(self) -> None:
         self.nlp = spacy.load("en_core_web_sm")
 
-    def _semantic_segmentation_core(
-        self, segments: List[str], threshold: float
-    ) -> dict:
+    def _semantic_segmentation_core(self, segments: List[str], threshold: float) -> dict:
         segment_map = [0]
         list_sim = [0.0]
         for index, (text1, text2) in enumerate(zip(segments[:-1], segments[1:])):
@@ -76,9 +64,7 @@ class SemanticTextSegmentation(BaseModel):
 
     def _get_max_segment_text(self, list_text: list) -> int:
         len_text = len(reduce(lambda a, b: a + b, list_text))
-        len_seg_text = int(
-            len_text / self.config_manager.config.model.segmentation.segment_num
-        )
+        len_seg_text = int(len_text / self.config_manager.config.model.segmentation.segment_num)
         if self.max_segment_text > len_seg_text:
             return max(len_seg_text - 500, int(self.max_segment_text / 2) + 1000)
         else:
@@ -89,9 +75,7 @@ class SemanticTextSegmentation(BaseModel):
         is_over = True
 
         while is_over:
-            dict_result = self._semantic_segmentation_core(
-                segments=segments, threshold=threshold
-            )
+            dict_result = self._semantic_segmentation_core(segments=segments, threshold=threshold)
             list_index = dict_result["list_index"]
             list_sim = dict_result["list_sim"]
             for index_i in list_index:
@@ -125,16 +109,12 @@ class SemanticTextSegmentation(BaseModel):
 
     def _get_similarity(self, text1: str, text2: str) -> float:
         """テキスト1とテキスト2の文書ベクトルのコサイン類似度を計算"""
-        sentence_1 = [
-            i.text.strip() for i in self.nlp(text1).sents if len(i.text.split(" ")) > 1
-        ]
-        sentence_2 = [
-            i.text.strip() for i in self.nlp(text2).sents if len(i.text.split(" ")) > 2
-        ]
+        sentence_1 = [i.text.strip() for i in self.nlp(text1).sents if len(i.text.split(" ")) > 1]
+        sentence_2 = [i.text.strip() for i in self.nlp(text2).sents if len(i.text.split(" ")) > 2]
         embeding_1 = self.model.encode(sentence_1)
         embeding_2 = self.model.encode(sentence_2)
-        embeding_1 = np.mean(embeding_1, axis=0).reshape(1, -1)
-        embeding_2 = np.mean(embeding_2, axis=0).reshape(1, -1)
+        embeding_1 = embeding_1.reshape(1, -1)
+        embeding_2 = embeding_2.reshape(1, -1)
 
         if np.any(np.isnan(embeding_1)) or np.any(np.isnan(embeding_2)):
             return 1
