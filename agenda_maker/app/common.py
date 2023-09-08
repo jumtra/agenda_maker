747bbd8346c72374d2729617066f4bf897784b3d
jumtras <data0science0acc@gmail.com>
2023-09-08 14:12:54 +0900

    refactor : del apps

--- agenda_maker/app/common.py
+++
@@ -1,5 +0,0 @@
-ALLOWED_EXTENSIONS = {"wav", "mp3", "mp4"}
-
-
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
--- agenda_maker/app/input.py
+++
@@ -1,5 +0,0 @@
-import gradio as gr
-
-
-def get_upload_component():
-    return gr.File(label="Upload Audio/Video [wav,mp3,mp4]")
